d3e7e1ee40a39a40d534bb9175c222d5b6f42992
Amlan Tarat <amlan.tarat@gmail.com>
2024-03-19 17:17:06 +0000

    Disable auto web secuirty

--- src/main/java/com/search/app/HospitalApplication.java
+++ src/main/java/com/search/app/HospitalApplication.java
@@ -2,13 +2,14 @@ package com.search.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @ComponentScan
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class HospitalApplication {
 
 	public static void main(String[] args) {
